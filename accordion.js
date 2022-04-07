635b0763e16008ed6f5a9b027e5f3cc556d52d38
Andrey Pavlenko <andreyp8391@gmail.com>
2022-04-08 00:18:11 +0500

    accordion

--- accordion.js
+++ accordion.js
@@ -1,27 +1,30 @@
-const accordions = document.querySelectorAll('.accordion');
+const accordion = () => {
+  const accordions = document.querySelectorAll(".accordion");
 
-accordions.forEach(item => {
-    item.addEventListener('click', (e) => {
-        if (e.target.classList.contains('accordion__button')) {
-            const accordionItems = Array.from( e.target.closest('.accordion').children );
-            const accordionActive = e.target.closest('.accordion__item');
-            
-            function showOneSlide() {
-                accordionItems.forEach(item => {
-                    item.classList.remove('is-active');         
-                })       
-                accordionActive.classList.add('is-active');
-            }
+  accordions.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      if (e.target.classList.contains("accordion__button")) {
+        const accordionItems = Array.from(
+          e.target.closest(".accordion").children
+        );
+        const accordionActive = e.target.closest(".accordion__item");
 
-            function toggleSlide() {                
-                accordionActive.classList.toggle('is-active')
-            }
-            
-            showOneSlide()
-            // toggleSlide()                    
-        }   
-    })
-})    
+        function showOneSlide() {
+          accordionItems.forEach((item) => {
+            item.classList.remove("is-active");
+          });
+          accordionActive.classList.add("is-active");
+        }
 
+        function toggleSlide() {
+          accordionActive.classList.toggle("is-active");
+        }
 
+        showOneSlide();
+        // toggleSlide()
+      }
+    });
+  });
+};
 
+export default accordion;
